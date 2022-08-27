60f792565201c3ea223e2e74d249edfad36ee90b
Divyaraj-Mandale <111412336+Divyaraj-Mandale@users.noreply.github.com>
2022-08-27 23:30:36 +0530

    Update Locationweb1Application.java

--- src/main/java/com/locationweb/Locationweb1Application.java
+++ src/main/java/com/locationweb/Locationweb1Application.java
@@ -2,30 +2,18 @@ package com.locationweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.locationweb.dto.LocationData;
-import com.locationweb.entities.Locations;
-import com.locationweb.services.LocationService;
 
 @SpringBootApplication
 @Controller
 public class Locationweb1Application {
 
 
-	@Autowired
-	private LocationService locationService;
-	
-	@Autowired
-	private EmailService emailService;
-	
-	//handler method
 	
  	@RequestMapping("/show")
 	public String showLocationPage() {
